f58320ceb53ccc2ef71fcada8feb75937e7b7112
Kiryunin <kiryunin@yandex.ru>
2023-10-23 01:01:06 +0300

    1

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -6,11 +6,6 @@ namespace WebAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -19,15 +14,15 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+            _logger.LogInfo("¬от информационное сообщение от нашего контроллера значений.");
+           
+            _logger.LogDebug("¬от отладочное сообщение от нашего контроллера значений.");
+           
+            _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений.");
+           
+            _logger.LogError("¬от сообщение об ошибке от нашего контроллера значений.");
+            return new string[] { "value1", "value2" }
     }
 }
